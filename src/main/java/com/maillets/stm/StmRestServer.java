f4910dc0ec9c1f699457f070abe8a8d6e4b9ab35
Marc-Andre Beaudry <marcandre.beaudry2@gmail.com>
2015-12-12 13:02:28 -0500

    Streaming seed

--- src/main/java/com/maillets/stm/StmRestServer.java
+++ src/main/java/com/maillets/stm/StmRestServer.java
@@ -1,7 +1,5 @@
 package com.maillets.stm;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.maillets.stm.entities.Stop;
-import com.maillets.stm.repository.StopRepository;
-
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
@@ -23,16 +18,14 @@ public class StmRestServer {
 	private static final Logger logger = LoggerFactory.getLogger(StmRestServer.class);
 
 	@Autowired
-	StopRepository stopRepository;
+	StopSeedLoader stopSeedLoader;
 
 	@Bean
 	CommandLineRunner init() {
 
 		return arg -> {
+			stopSeedLoader.load("/stops.txt");
 			logger.info("Init done!");
-			List<Stop> stops = StopSeedLoader.load("/stops.txt");
-			stopRepository.save(stops);
-			stopRepository.flush();
 		};
 	}
 
--- src/main/java/com/maillets/stm/StopSeedLoader.java
+++ src/main/java/com/maillets/stm/StopSeedLoader.java
@@ -4,48 +4,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.maillets.stm.entities.Stop;
+import com.maillets.stm.repository.StopRepository;
 
+@Component
 public class StopSeedLoader {
 
 	private static final Logger logger = LoggerFactory.getLogger(StopSeedLoader.class);
 
-	public static List<Stop> load(String source) {
+	@Autowired
+	StopRepository stopRepository;
 
-		List<Stop> stops = new ArrayList<Stop>();
+	public void load(String source) {
+
 		InputStream is = StopSeedLoader.class.getResourceAsStream(source);
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-			Stream<Stop> stopStream = br.lines().skip(1).map(mapToStop).filter(x -> x != null);
-			stops = stopStream.collect(Collectors.toList());
+			br.lines().skip(1).forEach(line -> {
+				Stop stop = new Stop();
+				String[] splittedLine = line.split(",");
+				stop.setId(Integer.parseInt(splittedLine[0]));
+				stop.setCode(Integer.parseInt(splittedLine[1]));
+				stop.setName(splittedLine[2]);
+				stop.setLatitude(splittedLine[3]);
+				stop.setLongitude(splittedLine[4]);
+				stop.setUrl(splittedLine[5]);
+				stop.setWheelchairBoarding(splittedLine[6]);
+				stopRepository.saveAndFlush(stop);
+			});
 		} catch (IOException ex) {
 			logger.error(ex.getMessage(), ex);
 		}
-		return stops;
 	}
-
-	private static Function<String, Stop> mapToStop = (line) -> {
-		Stop stop = new Stop();
-		try {
-			String[] splittedLine = line.split(",");
-			stop.setId(Integer.parseInt(splittedLine[0]));
-			stop.setCode(Integer.parseInt(splittedLine[1]));
-			stop.setName(splittedLine[2]);
-			stop.setLatitude(splittedLine[3]);
-			stop.setLongitude(splittedLine[4]);
-			stop.setUrl(splittedLine[5]);
-			stop.setWheelchairBoarding(splittedLine[6]);
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-		return stop;
-	};
 }
